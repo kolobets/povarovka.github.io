220889376e0d7bc9d39a6cb30257c4ae9f8cbb1f
kolobets <54442800+kolobets@users.noreply.github.com>
2019-08-23 16:43:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Поваровка</title>
+
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Поваровка">
+<meta name="keywords" content="поваровка, поварово, локомотивный, школа им. 8 марта, д/c елочка" >
+
+<script type="text/javascript" src="/js/script.js"></script>
+<link rel="stylesheet" type="text/css" href="/css/style.css">
+
+</head>
+<body>
+TEST
+<body>
+</html>
